8bfcceb35075419c03e1647491e9dcf7c891adef
Maolaohei <24179463+Maolaohei@users.noreply.github.com>
2026-06-19 13:36:44 +0000

    chore: 自动格式化 Go 代码 (gofmt 自动修复)

--- transport/internet/warmup_pipeline_test.go
+++ transport/internet/warmup_pipeline_test.go
@@ -34,8 +34,8 @@ type mockConn struct {
 func (c *mockConn) Read(b []byte) (int, error)         { return 0, nil }
 func (c *mockConn) Write(b []byte) (int, error)        { return len(b), nil }
 func (c *mockConn) Close() error                       { return nil }
-func (c *mockConn) LocalAddr() stdnet.Addr              { return c.addr }
-func (c *mockConn) RemoteAddr() stdnet.Addr             { return c.addr }
+func (c *mockConn) LocalAddr() stdnet.Addr             { return c.addr }
+func (c *mockConn) RemoteAddr() stdnet.Addr            { return c.addr }
 func (c *mockConn) SetDeadline(t time.Time) error      { return nil }
 func (c *mockConn) SetReadDeadline(t time.Time) error  { return nil }
 func (c *mockConn) SetWriteDeadline(t time.Time) error { return nil }
